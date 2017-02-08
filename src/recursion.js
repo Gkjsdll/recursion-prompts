5252f88a2993087a187e5a3d736cda699c8e7777
Zack Winchell <zackwinchell@gmail.com>
2017-02-08 15:12:14 -0800

    Implement augmentElements

--- src/recursion.js
+++ src/recursion.js
@@ -522,6 +522,11 @@ var compress = function(list) {
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+  array[0].push(aug);
+  if (array.length > 1) {
+    augmentElements(array.slice(1), aug);
+  }
+  return array;
 };
 
 // 34. Reduce a series of zeroes to a single 0.
