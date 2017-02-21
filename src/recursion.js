2a6151fb174a7e37ebce0b2732f532b16ecf093e
Zack Winchell <zackwinchell@gmail.com>
2017-02-21 15:51:50 -0800

    Implement gcd

--- src/recursion.js
+++ src/recursion.js
@@ -231,6 +231,26 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0) {
+    return null;
+  }
+  
+  if (!x) {
+    return y;
+  }
+
+  if (!y) {
+    return x;
+  }
+
+  if (x < y) {
+    return gcd(y, x);
+  }
+
+  let quotient = Math.floor(x/y);
+  let remainder = x % y;
+
+  return gcd(y, remainder);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
