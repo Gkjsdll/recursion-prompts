4d8771c5faed876a8257cc957af455696257f9a2
Zack Winchell <zackwinchell@gmail.com>
2017-03-28 12:47:07 -0700

    Clean up letterTally

--- src/recursion.js
+++ src/recursion.js
@@ -508,11 +508,7 @@ var flatten = function(array) {
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
-var letterTally = function(str, obj) {
-  if (obj === undefined) {
-    obj = {};
-  }
-
+var letterTally = function(str, obj = {}) {
   if (str.length) {
     let char = str.charAt(0);
 
@@ -521,9 +517,9 @@ var letterTally = function(str, obj) {
     }
     obj[char]++;
     letterTally(str.slice(1), obj);
-
-    return obj;
   }
+  
+  return obj;
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
